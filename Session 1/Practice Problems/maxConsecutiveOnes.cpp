feeca63a621d3647356331ceb1a5d68a36f4a0c6
RinkuMonani <monanira@gmail.com>
2020-07-04 19:53:46 +0530

    Added code from session

--- Session 1/Practice Problems/maxConsecutiveOnes.cpp
+++ Session 1/Practice Problems/maxConsecutiveOnes.cpp
@@ -10,3 +10,37 @@
 //  3
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int getMaxConsecutiveOnes(int A[], int size){
+    int maxOnes = 0;
+    int temp = 0;
+
+    for(int i=0; i<size; ++i){
+        if(A[i] == 1){
+            temp++;
+        }
+        else if(A[i] == 0){
+            temp = 0;
+        }
+       
+       maxOnes = max(temp, maxOnes); // min(a, b)
+
+    }
+
+    return maxOnes;
+}
+
+//driver code
+int main(){
+
+    int binaryArray[] {1 ,1, 1, 1, 1, 0, 1, 1, 1};
+    int size = sizeof(binaryArray) / sizeof(binaryArray[0]);
+    int result = getMaxConsecutiveOnes(binaryArray, size);
+    cout<<result<<"\n";
+
+
+    return 0;
+}
