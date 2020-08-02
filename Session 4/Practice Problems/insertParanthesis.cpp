04cde7dd316ebbc388dce7cb0c74f72dd7462874
RinkuMonani <monanira@gmail.com>
2020-08-02 15:41:26 +0530

    added link to question

---
+++ Session 4/Practice Problems/insertParanthesis.cpp
@@ -0,0 +1,45 @@
+/*
+
+    Question: Given a string of digits S, insert a minimum number of opening and closing parentheses into it 
+              such that the resulting string is balanced and each digit d is inside exactly d pairs of matching parentheses.
+
+    LINK: https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd27/0000000000209a9f
+    SOURCE: Google Code Jam Qualification Round 2020
+
+    Input:
+    "1234"
+
+    Output:
+    (1(2(3(4))))
+
+    231
+
+    string eq = "231"
+
+    int open = 1
+
+    ((2(3))1)
+
+
+    string eq = "894"
+
+    int open = 4
+
+    string output = "((((((((8(9)))))4))))"cls
+    
+
+
+    2[a]
+
+    aa
+
+    2[ a 3[b] ] -> abbbabbb
+
+
+
+
+
+
+*/
+
+
