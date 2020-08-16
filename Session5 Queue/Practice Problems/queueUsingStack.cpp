1d051d92be695c62730a334132941e8fd0d3cf96
RinkuMonani <monanira@gmail.com>
2020-08-16 19:28:02 +0530

    added link to queue using stack question

--- Session5 Queue/Practice Problems/queueUsingStack.cpp
+++ Session5 Queue/Practice Problems/queueUsingStack.cpp
@@ -1,7 +1,11 @@
 /*
-    Topic: Quueu using Stack
+    Topic: Queue using Stack
     Question: https://practice.geeksforgeeks.org/problems/queue-using-two-stacks/1
 
     Topic: Stack using Queue
     Question: https://practice.geeksforgeeks.org/problems/stack-using-two-queues/1
+
+    stack -> queue
+
+
 */
